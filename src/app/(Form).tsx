0321ce12d6f2edad06d69cbc71df41a0953a1ec8
Wulfre <wulfre@noreply.users.github.com>
2023-09-23 13:44:52 -0400

    improve observable usage on form

--- src/app/(Form).tsx
+++ src/app/(Form).tsx
@@ -5,27 +5,32 @@ import { observable } from "@legendapp/state"
 import { $favorites } from "~/stores/favorites"
 import { $user } from "~/stores/user"
 
-const $username = observable<string>("")
+const $username = observable("")
 
-const Form: FunctionComponent = () => (
-    <div className={"flex flex-col gap-4"} data-testid={"form"}>
-        <input
-            className={"c-background bg-foreground outline-none h-4ch p-x-2 b-rd-1 focus:outline-blue"}
-            data-testid={"form--username"}
-            onInput={(event) => { $username.set(event.currentTarget.value) }}
-            type={"text"}
-            value={$username.use()} />
-        <button
-            className={"bg-blue c-black b-rd-1 h-4ch"}
-            disabled={$user.state.loading.use()}
-            onClick={async () => {
-                await $user.actions.fetchUser($username.get())
-                void $favorites.actions.fetchFavorites($user.state.data.id.get())
-            }}
-        >
-            {"Go"}
-        </button>
-    </div>
-)
+const Form: FunctionComponent = () => {
+    const username = $username.use()
+
+    return (
+        <div className={"flex flex-col gap-4"} data-testid={"form"}>
+            <input
+                className={"c-background bg-foreground h-4ch p-x-2 b-rd-1 outline-none focus:outline-blue"}
+                data-testid={"form--username"}
+                onInput={(event) => { $username.set(event.currentTarget.value) }}
+                type={"text"}
+                value={username}
+            />
+            <button
+                className={"bg-blue c-black b-rd-1 h-4ch"}
+                disabled={$user.state.loading.use()}
+                onClick={async () => {
+                    await $user.actions.fetchUser(username)
+                    void $favorites.actions.fetchFavorites($user.state.data.id.get())
+                }}
+            >
+                {"Go"}
+            </button>
+        </div>
+    )
+}
 
 export default Form
