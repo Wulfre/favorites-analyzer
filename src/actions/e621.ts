5a6926af24b3dc0f9eb991b3499a5de75c7be1ef
Wulfre <wulfre@noreply.users.github.com>
2023-09-23 20:16:42 -0400

    remove unused constants file, split getFavorites into two functions

--- src/actions/e621.ts
+++ src/actions/e621.ts
@@ -7,14 +7,17 @@ import type { User } from "~/schemas/user"
 import { log } from "~/lib/log"
 import { postSchema } from "~/schemas/post"
 import { userSchema } from "~/schemas/user"
-import { e621ClientString } from "~/utils/constants"
 
-export const getUser = async (username: string): Promise<User | undefined> => {
-    const response = await fetch(`https://e621.net/users/${encodeURIComponent(username)}.json?_client=${e621ClientString}`, {
-        next: {
-            revalidate: 60 * 60,
-        },
-    })
+const throttle = pThrottle({
+    limit: 2,
+    interval: 1000,
+})
+
+const clientString = encodeURIComponent("Favorites Analyzer/0.1 (by Wulfre)")
+const pageLimit = 300
+
+export const getUser = throttle(async (username: string): Promise<User | undefined> => {
+    const response = await fetch(`https://e621.net/users/${encodeURIComponent(username)}.json?_client=${clientString}`, { next: { revalidate: 60 * 60 } })
 
     if (!response.ok) {
         log.debug(`failed to fetch user data for user "${username}"`)
@@ -25,40 +28,32 @@ export const getUser = async (username: string): Promise<User | undefined> => {
     log.debug(`fetched user data for user "${username}", id ${user.id}`)
 
     return user
-}
+})
 
-export const getFavorites = async (user: User): Promise<Post[] | undefined> => {
+const getFavoritesPage = throttle(async (userId: number, page: number): Promise<Post[]> => {
     const responseSchema = z.object({
         posts: z.array(postSchema),
     })
 
-    const throttle = pThrottle({
-        limit: 2,
-        interval: 1000,
-    })
+    const response = await fetch(`https://e621.net/favorites.json?_client=${clientString}&user_id=${userId}&limit=${pageLimit}&page=${page}`, { next: { revalidate: 60 * 60 } })
 
-    const pageLimit = 300
+    if (!response.ok) {
+        log.debug(`failed to fetch page ${page} of favorites for user ${userId}`)
+        return []
+    }
+
+    const { posts } = responseSchema.parse(await response.json())
+    log.debug(`fetched page ${page} of favorites for user ${userId}`)
+    return posts
+})
+
+export const getFavorites = async (user: User): Promise<Post[]> => {
     const pages = Math.ceil(user.favorite_count / pageLimit)
 
     const requests = Array.from(
         { length: pages }, // create an array of length `pages`
         (_, index) => index + 1, // fill it with numbers from 1 to `pages`
-    ).map(throttle(async (page) => {
-        const response = await fetch(`https://e621.net/favorites.json?_client=${e621ClientString}&user_id=${user.id}&limit=${pageLimit}&page=${page}`, {
-            next: {
-                revalidate: 60 * 60,
-            },
-        })
-
-        if (!response.ok) {
-            log.debug(`failed to fetch page ${page} of favorites for user ${user.id}`)
-            return []
-        }
-
-        const { posts } = responseSchema.parse(await response.json())
-        log.debug(`fetched page ${page} of favorites for user ${user.id}, ${posts.length} posts`)
-        return posts
-    }))
+    ).map(async (page) => getFavoritesPage(user.id, page))
 
     const pagesData = await Promise.allSettled(requests)
     return pagesData.flatMap((page) => page.status === "fulfilled" ? page.value : [])
--- src/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const e621ClientString = encodeURIComponent("Favorites Analyzer/0.1 (by Wulfre)")
-
