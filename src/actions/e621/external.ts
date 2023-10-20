5ed03aac4dd5914d6d28a7c27a83bac56408ce49
Wulfre <wulfre@noreply.users.github.com>
2023-10-20 18:16:20 -0400

    remove log util, upgrade dependencies

--- src/actions/e621/external.ts
+++ src/actions/e621/external.ts
@@ -5,7 +5,6 @@ import { z } from "zod"
 import { clientString, pageLimit } from "./config"
 import type { Post } from "~/schemas/post"
 import type { User } from "~/schemas/user"
-import { log } from "~/utils/log"
 import { postSchema } from "~/schemas/post"
 import { userSchema } from "~/schemas/user"
 
@@ -21,17 +20,17 @@ export const getUser = throttle(async (username: string): Promise<User | undefin
         const response = await fetch(`https://e621.net/users/${encodeURIComponent(username)}.json?_client=${clientString}`, { cache: "no-cache" })
 
         if (!response.ok) {
-            log.warn(`failed to fetch user data for user "${username}"`)
+            console.warn(`failed to fetch user data for user "${username}"`)
             return defaultReturn
         }
 
         const user = userSchema.parse(await response.json())
-        log.info(`fetched user data for user "${username}", id ${user.id}`)
+        console.info(`fetched user data for user "${username}", id ${user.id}`)
 
         return user
     }
     catch {
-        log.error(`failed to fetch user data for user "${username}"`)
+        console.error(`failed to fetch user data for user "${username}"`)
         return defaultReturn
     }
 })
@@ -47,16 +46,16 @@ export const getFavoritesPage = throttle(async (userId: number, page: number): P
         const response = await fetch(`https://e621.net/favorites.json?_client=${clientString}&user_id=${userId}&limit=${pageLimit}&page=${page}`, { cache: "no-cache" })
 
         if (!response.ok) {
-            log.warn(`failed to fetch page ${page} of favorites for user ${userId}`)
+            console.warn(`failed to fetch page ${page} of favorites for user ${userId}`)
             return defaultReturn
         }
 
         const { posts } = responseSchema.parse(await response.json())
-        log.info(`fetched page ${page} of favorites for user ${userId}`)
+        console.info(`fetched page ${page} of favorites for user ${userId}`)
         return posts
     }
     catch {
-        log.error(`failed to fetch page ${page} of favorites for user ${userId}`)
+        console.error(`failed to fetch page ${page} of favorites for user ${userId}`)
         return defaultReturn
     }
 })
