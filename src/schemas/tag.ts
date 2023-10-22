c58f99607e88d3c2db9dad38c0761e5ff8fdb8b8
Wulfre <wulfre@noreply.users.github.com>
2023-10-21 21:09:13 -0400

    move dotenv to dev dependencies

--- src/schemas/tag.ts
+++ src/schemas/tag.ts
@@ -1,14 +1,14 @@
 import { z } from "zod"
 
 export const tagSchema = z.object({
-    category: z.number(),
-    created_at: z.string(),
     id: z.number(),
-    is_locked: z.boolean(),
     name: z.string(),
     post_count: z.number(),
     related_tags: z.string(),
     related_tags_updated_at: z.string(),
+    category: z.number(),
+    is_locked: z.boolean(),
+    created_at: z.string(),
     updated_at: z.string(),
 })
 export type Tag = z.infer<typeof tagSchema>
